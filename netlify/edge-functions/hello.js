3431d7a22f7d364d86eb8833c4d407516ffe0136
fxadilima <107688563+fxadilima@users.noreply.github.com>
2023-05-06 02:48:31 +0700

    Add files via upload

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,4 +1,7 @@
-export default () => new Response("Hello world");
+export default (req) => {
+    console.log("request: " + req);
+    new Response("Hello world");
+}
 
 export const config = { path: "/test" };
 
